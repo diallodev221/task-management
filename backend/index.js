ee9a364f35cdd0e07d61a1c112f6ccaa7a068459
diallodeveloper <diallocodeur@gmail.com>
2023-05-05 10:22:03 +0000

    move the README.md file in the root folder

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express =  require("express");
 const cors = require('cors');
 require('dotenv').config();
 
-const router = require("./routes/task");
+const taskRouter = require("./routes/task");
 const connectDB = require('./db');
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -20,21 +20,20 @@ app.use(cors({ origin: 'http://localhost:3000'}))
 // express json parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(notFound)
+// app.use(notFound)
 app.use(errorHandlerMiddleware)
 
 
 
 //router
-app.get("/", (req, res) => res.send("Task Manager App"))
-app.use("/api/v1/tasks", router);
+app.get("/welcome", (req, res) => res.send("Task Manager App"))
+
+app.use("/api/v1/tasks", taskRouter);
 
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    // console.log('URI: ' +process.env)
-    console.log('PORT: ' +process.env.PORT)
     app.listen(PORT, console.log(`App is running on port: http://localhost:${PORT}`)
     )
   } catch (error) {
